a218e8e3a94ec863a8828106cc5ee01a90fed922
Cristian F. Dávila <cristian.davila@globant.com>
2022-05-27 10:47:35 -0500

    iframe complete

--- test/specs/iframe.js
+++ test/specs/iframe.js
@@ -3,10 +3,19 @@ describe('Iframe Tests', () =>{
     it('Working with iframe', async () => {
         await browser.url('/iframe-sample/');
 
+        // check page title
+        await expect($('h1.tg-page-header__title')).toHaveText('IFrame Sample');
+
         // change the context to the iframe
         const iframe = await $('#advanced_iframe');
         await browser.switchToFrame(iframe);
 
         await expect($('#site-logo')).toExist();
+
+        // return to the main frame
+        await browser.switchToParentFrame();
+
+        // check page title
+        await expect($('h1.tg-page-header__title')).toHaveText('IFrame Sample');
     });
 });
